8b72f052752114b64248715d1664931be87fe996
Encarni Vicente <encvicgal@alu.edu.gva.es>
2024-11-24 20:33:09 +0100

    first commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+Mi repositorio. Por: 
+Encarni Vicente.
+Ejemplo de entornos de desarrollo 
+para colaborar en un repositorio local.
---
+++ src/proyecto/principal.java
@@ -0,0 +1,8 @@
+package proyecto;
+
+public class principal {
+    //verificamos que se haya creado correctamente el repositorio
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+}
